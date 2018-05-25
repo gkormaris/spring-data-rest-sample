fab1b8b12105da1a7c47e90cf99d8d2d6a810aa1
Ioannis Kormaris <v-ikormaris@eurobank.gr>
2018-05-25 16:39:17 +0300

    pretty json

--- src/main/java/gr/codeschool/spring/sample/SpringSampleApplication.java
+++ src/main/java/gr/codeschool/spring/sample/SpringSampleApplication.java
@@ -2,12 +2,19 @@ package gr.codeschool.spring.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 public class SpringSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSampleApplication.class, args);
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder().indentOutput(true);
     }
 
 }
