ff214d9b992be0b259fb45ead563f678c830e46d
gkormaris <gkormaris@gmail.com>
2020-10-22 19:38:57 +0300

    removed data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,4 +3,3 @@ insert into department (id, name, building) values (2, 'IT', 'B1');
 
 insert into employee (id, name, age, email, dep_id) values (1001, 'Joe', 32, 'joe@mail.com', select id from department where name = 'Sales');
 insert into employee (id, name, age, email, dep_id) values (1002, 'Sam', 28, 'sam@mail.com', select id from department where name = 'IT');
-insert into employee (id, name, age, email, dep_id) values (1003, 'John', 43, 'john@mail.com', select id from department where name = 'IT');
