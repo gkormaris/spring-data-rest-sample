ca1a0e805ae31dd3752d93a73f31f2015b9e9465
gkormaris <gkormaris@gmail.com>
2020-10-27 10:37:34 +0200

    added cross origin

--- src/main/java/gr/codeschool/spring/sample/SpringSampleApplication.java
+++ src/main/java/gr/codeschool/spring/sample/SpringSampleApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @SpringBootApplication
 public class SpringSampleApplication {
 
